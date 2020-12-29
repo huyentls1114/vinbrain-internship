4ca21c604bc8801c3281b22329eedd970ee291b0
huyen <huyentls1114@gmail.com>
2020-12-30 03:47:14 +0700

    add radam

--- classifier/config/configs_colabs.py
+++ classifier/config/configs_colabs.py
@@ -8,6 +8,7 @@ from model.CNN import CNN
 from torch.optim import SGD
 from torch.optim.lr_scheduler import StepLR, MultiStepLR, ReduceLROnPlateau
 from utils.utils import len_train_datatset
+from model.optimizer import RAdam
 
 config_files = "/content/drive/MyDrive/vinbrain_internship/vinbrain-internship/classifier/config/configs_colabs.py"
 #data config
