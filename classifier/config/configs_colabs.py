01bbacd967153dab0b34150e8e30f38b06044ad3
huyen <huyentls1114@gmail.com>
2020-12-31 18:31:38 +0700

    colab_config

--- classifier/config/configs_colabs.py
+++ classifier/config/configs_colabs.py
@@ -52,7 +52,7 @@ net = {
     }
 }
 loss_function = nn.CrossEntropyLoss
-lr = 0.001
+lr = 0.01
 steps_per_epoch = int(len_train_datatset(dataset, transform_train, split_train_val)/batch_size)
 # lr_schedule = {
 #     "class": StepLR,
