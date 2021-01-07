779fdc3c542542d9a3c47e3a1f1b809104601311
huyen <huyentls1114@gmail.com>
2021-01-07 16:43:44 +0700

    modify config colabs

---
+++ segmentation/config/config_colab.py
@@ -0,0 +1,92 @@
+import torch.nn as nn
+import torchvision.transforms as transforms
+from torch.optim import Adam
+from torch.optim.lr_scheduler import ReduceLROnPlateau
+
+from dataset.BrainTumorDataset import BrainTumorDataset
+from model.metric import Dice_Score
+from model.Unet import Unet
+
+#data config
+image_size = 256
+transform_train = transforms.Compose([
+    transforms.ToPILImage(),
+    transforms.Resize(image_size),
+    transforms.ToTensor()
+])
+transform_test = transforms.Compose([
+    transforms.ToPILImage(),
+    transforms.Resize(image_size),
+    transforms.ToTensor()
+
+])
+transform_label = transforms.Compose([
+    transforms.ToPILImage(),
+    transforms.Resize(image_size),
+    transforms.ToTensor()
+])
+
+dataset = {
+    "class": BrainTumorDataset,
+    "dataset_args":{
+        "input_folder":"/content/data/BrainTumor"
+    }
+}
+
+#train config
+net = {
+    "class":Unet,
+    "net_args":{
+        "input_channel":1,
+        "output_channel":1,
+        "batch_norm": True,
+        "padding" : 1,
+        "bilinear": True
+    }
+}
+
+device = "cpu"
+gpu_id = 0
+
+batch_size = 16
+num_epochs = 20
+
+metric = {
+    "class":Dice_Score,
+    "metric_args":{
+        "threshold":0.5,
+        "epsilon":1e-4
+    }
+}
+num_classes = 1
+loss_function = {
+    "class": nn.BCEWithLogitsLoss,
+    "loss_args":{
+    }
+}
+
+output_folder = "/content/drive/MyDrive/vinbrain_internship/model/BrainTumor"
+loss_file = "loss_file.txt"
+config_file_path = "/content/vinbrain-internship/segmentation/config/config_colab.py"
+
+#optimizer
+lr = 1e-3
+optimizer = {
+    "class":Adam,
+    "optimizer_args":{
+    }
+}
+lr_scheduler = {
+    "class": ReduceLROnPlateau,
+    "metric":"val_loss",
+    "step_type":"epoch",
+    "schedule_args":{
+        "mode":"min",
+        "factor":0.5,
+        "patience":4,
+        "threshold":1e-2,
+        "min_lr":1-5
+    }
+}
+steps_save_loss = 100
+steps_save_image = 100
