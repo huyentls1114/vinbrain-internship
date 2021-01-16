3fbdb94e7665bad4a9d072cf47d865a9eb0b0f18
huyen <huyentls1114@gmail.com>
2021-01-16 15:30:17 +0700

    modify get len dataset

--- segmentation/config/config_colab.py
+++ segmentation/config/config_colab.py
@@ -88,7 +88,7 @@ optimizer = {
 from torch.optim.lr_scheduler import OneCycleLR
 steps_per_epoch = int(len_train_datatset(dataset, transform_train, transform_label, 1)/batch_size)
 num_epochs = 30
-lr_schedule = {
+lr_scheduler = {
     "class":OneCycleLR,
     "metric": None,
     "step_type":"batch",
