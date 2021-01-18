3c68244c2781db3ac8ee04daa7a005026f4bf158
huyen <huyentls1114@gmail.com>
2021-01-18 10:55:49 +0700

    focal loss2

--- segmentation/loss/loss.py
+++ segmentation/loss/loss.py
@@ -28,7 +28,7 @@ class FocalLoss(nn.Module):
         return sigmoid_focal_loss(inputs, targets, self.alpha, self.gamma, reduction="mean")
 class FocalLossWithLogits(nn.Module):
     def __init__(self, alpha = 0.25, gamma = 2):
-        super(FocalLoss, self).__init__()
+        super(FocalLossWithLogits, self).__init__()
         self.alpha = alpha
         self.gamma = gamma
     def forward(self, inputs, targets):
