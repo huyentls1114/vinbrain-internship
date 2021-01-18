b8e15db889fd8f01a45eeee040d71f6f54a08d10
huyen <huyentls1114@gmail.com>
2021-01-19 04:07:59 +0700

    turning efficient net

--- segmentation/config/config_colab.py
+++ segmentation/config/config_colab.py
@@ -97,7 +97,7 @@ lr_scheduler = {
     "metric": None,
     "step_type":"batch",
     "schedule_args":{
-        "max_lr":0.01,
+        "max_lr":0.009,
         "epochs":num_epochs,
         "steps_per_epoch":steps_per_epoch+1
     }    
