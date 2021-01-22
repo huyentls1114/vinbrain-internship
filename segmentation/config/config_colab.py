a2ed27c055f92fab82a7f89ae1d27a8075bd450f
huyen <huyentls1114@gmail.com>
2021-01-22 17:50:59 +0700

    BackboneResnet34VGG_BCE_noargument

--- segmentation/config/config_colab.py
+++ segmentation/config/config_colab.py
@@ -38,12 +38,12 @@ dataset = {
 }
 
 #train config
-from model.backbone import BackboneResnet18VGG
+from model.backbone import BackboneResnet34VGG
 num_classes = 1
 net = {
     "class":Unet,
     "net_args":{
-        "backbone_class": BackboneResnet18VGG,
+        "backbone_class": BackboneResnet34VGG,
         "encoder_args":{
             "pretrained":True           
         },
@@ -74,7 +74,7 @@ loss_function = {
     }
 }
 
-output_folder = "/content/drive/MyDrive/vinbrain_internship/model_BrainTumor/BackboneResnet18VGG_BCE_noargument"
+output_folder = "/content/drive/MyDrive/vinbrain_internship/model_BrainTumor/BackboneResnet34VGG_BCE_noargument"
 loss_file = "loss_file.txt"
 config_file_path = "/content/vinbrain-internship/segmentation/config/config_colab.py"
 
