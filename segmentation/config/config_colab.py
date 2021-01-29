4c6e0fbbf35c5d56a1c0a298414d70cd661bfa74
huyen <huyentls1114@gmail.com>
2021-01-29 14:13:09 +0700

    pattern model

--- segmentation/config/config_colab.py
+++ segmentation/config/config_colab.py
@@ -13,7 +13,7 @@ from torch.optim.lr_scheduler import OneCycleLR
 import albumentations as A
 #data config
 image_size = 256
-output_folder = "/content/drive/MyDrive/vinbrain_internship/model_Pneumothorax/BackboneResnet34VGG_combine_augment_Plateau"
+output_folder = "/content/drive/MyDrive/vinbrain_internship/model_Pneumothorax/BackboneResnet34VGG_mixloss_metter_augment_Plateau"
 loss_file = "loss_file.txt"
 config_file_path = "/content/vinbrain-internship/segmentation/config/config_colab.py"
 
