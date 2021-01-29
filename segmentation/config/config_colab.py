2f1e3ee77e110dc131c9a2ae59c6b2a5212ca4e1
huyen <huyentls1114@gmail.com>
2021-01-29 10:48:23 +0700

    fix bug

--- segmentation/config/config_colab.py
+++ segmentation/config/config_colab.py
@@ -13,7 +13,7 @@ from torch.optim.lr_scheduler import OneCycleLR
 import albumentations as A
 #data config
 image_size = 256
-output_folder = "/content/drive/MyDrive/vinbrain_internship/model_Pneumothorax/BackboneResnet34VGG_diceloss_augment_Plateau"
+output_folder = "/content/drive/MyDrive/vinbrain_internship/model_Pneumothorax/BackboneResnet34VGG_focalloss_augment_Plateau"
 loss_file = "loss_file.txt"
 config_file_path = "/content/vinbrain-internship/segmentation/config/config_colab.py"
 
@@ -89,15 +89,15 @@ metric = {
         "epsilon":1e-4
     }
 }
-# num_classes = 1
-num_classes = 1
-from loss.loss import DiceLoss
+from loss.loss import FocalLoss
 loss_function = {
-    "class": DiceLoss,
+    "class": FocalLoss,
     "loss_args":{
-        "activation":nn.Sigmoid()
+        "alpha": 0.98,
+        "gamma": 2
     }
 }
+
 #optimizer
 lr = 1e-3
 optimizer = {
