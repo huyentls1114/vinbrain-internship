b785eb7d5ee1194e5cd64e0a64c7969f62096812
huyen <huyentls1114@gmail.com>
2021-02-01 02:50:11 +0700

    test won project

--- segmentation/config/config_colab.py
+++ segmentation/config/config_colab.py
@@ -63,7 +63,7 @@ dataset = {
 import os
 # from model.unet import UnetCRF
 # from model.backbone import BackboneResnet34VGG
-num_classes = 3
+num_classes = 1
 # net = {
 #     "class":Unet,
 #     "net_args":{
