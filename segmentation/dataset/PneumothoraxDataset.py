593c7c2c6565fb7ff490db2138b0fde00f6ccc55
huyen <huyentls1114@gmail.com>
2021-02-23 15:34:37 +0700

    modify show batch img

--- segmentation/dataset/PneumothoraxDataset.py
+++ segmentation/dataset/PneumothoraxDataset.py
@@ -113,7 +113,7 @@ class PneumothoraxDataset(Dataset):
                mask = np.concatenate([mask]*3, axis = 2)
             combine = np.hstack([img, mask, ct])
             list_combine.append(combine)
-        plt.imshow(np.vstack(list_combine)[:,:,0])
+        plt.imshow(np.vstack(list_combine)[:,:,0], cmap = "gray")
         plt.axis('off')
         plt.show()
 
