f847f5d3172da824356a29af0ecb17fed3ca527f
huyen <huyentls1114@gmail.com>
2021-02-25 17:08:53 +0700

    classify

---
+++ classifier/config/pattern_config.py
@@ -0,0 +1,288 @@
+#lr schedule
+lr_scheduler = {
+    "class": ReduceLROnPlateau,
+    "metric":"val_loss_avg",
+    "step_type":"epoch",
+    "schedule_args":{
+        "mode":"min",
+        "factor":0.1,
+        "patience":3,
+        "threshold":1e-2,
+        "min_lr":1-5
+    }
+}
+
+
+from torch.optim.lr_scheduler import OneCycleLR
+lr_scheduler = {
+    "class":OneCycleLR,
+    "metric": None,
+    "step_type":"batch",
+    "schedule_args":{
+        "max_lr": lr,
+        "epochs":num_epochs,
+        "steps_per_epoch":steps_per_epoch+1,
+    }    
+}
+
+
+from torch.optim.lr_scheduler import CosineAnnealingWarmRestarts
+lr_scheduler = {
+    "class":CosineAnnealingWarmRestarts,
+    "metric": None,
+    "step_type":"iteration",
+    "schedule_args":{
+        "T_0": 1,
+        "T_mult":2
+    }    
+}
+
+from torch.optim.lr_scheduler import StepLR
+lr_scheduler = {
+    "class": StepLR,
+    "metric":None,
+    "step_type":"epoch",
+    "schedule_args":{
+        "step_size":5,
+        "gamma":0.1
+    }
+}
+
+#OneCycleLR
+from torch.optim.lr_scheduler import OneCycleLR
+steps_per_epoch = int(len_train_datatset(dataset, transform_train, transform_label, 1)/batch_size)
+num_epochs = 80
+lr_scheduler = {
+    "class":OneCycleLR,
+    "metric": None,
+    "step_type":"batch",
+    "schedule_args":{
+        "max_lr": 1e-4,
+        "epochs":num_epochs,
+        "steps_per_epoch":steps_per_epoch+1,
+        "final_div_factor":10,
+    }    
+}
+
+# net
+num_classes = 1
+net = {
+    "class":AlbuNet,
+    "net_args":{
+        "pretrained":True
+    }
+}
+
+
+num_classes = 1
+from model.backbone import BackboneOriginal
+net = {
+    "class":Unet,
+    "net_args":{
+        "backbone_class": BackboneOriginal,
+        "encoder_args":{},
+        "decoder_args":{
+            "bilinear": True
+        }
+    }
+}
+net = {
+    "class":UnetDynamic,
+    "net_args":{
+        "backbone_class": BackBoneResnet101Dynamic,
+        "encoder_args":{
+            "pretrained":True           
+        },
+        "decoder_args":{
+            "img_size": image_size
+        }
+    }
+}
+net = {
+    "class":UnetDynamic,
+    "net_args":{
+        "backbone_class": BackBoneResnet18Dynamic,
+        "encoder_args":{
+            "pretrained":True           
+        },
+        "decoder_args":{
+            "img_size": image_size
+        }
+    }
+}
+net = {
+    "class":Unet,
+    "net_args":{
+        "backbone_class": BackboneResnet18,
+        "encoder_args":{
+            "pretrained":True           
+        },
+        "decoder_args":{
+            "bilinear": False,
+            "pixel_shuffle":True
+        }
+    }
+}
+from model.backbone_resnet import BackboneResnet18
+net = {
+    "class":Unet,
+    "net_args":{
+        "backbone_class": BackboneResnet18,
+        "encoder_args":{
+            "pretrained":True           
+        },
+        "decoder_args":{
+        }
+    }
+}
+num_classes = 1
+net = {
+    "class":Unet,
+    "net_args":{
+        "backbone_class": BackboneEfficientB0VGG,
+        "encoder_args":{
+            "pretrained":True           
+        },
+        "decoder_args":{
+            "bilinear": False,
+            "pixel_shuffle":True
+        }
+    }
+}
+
+from model.unet import UnetCRF
+from model.backbone import BackboneResnet18VGG
+num_classes = 1
+current_epoch = 99
+net = {
+    "class":UnetCRF,
+    "net_args":{
+        "checkpoint_path": os.path.join(output_folder, "checkpoint_"+str(current_epoch))
+        "backbone_class": BackboneResnet18VGG,
+        "encoder_args":{
+            "pretrained":True           
+        },
+        "decoder_args":{
+            "pixel_shuffle":True,
+            "bilinear":False
+        }
+    }
+}
+
+#metric
+metric = {
+    "class":Dice_Score,
+    "metric_args":{
+        "threshold":0.5,
+        "epsilon":1e-4
+    }
+}
+
+#loss function
+loss_function = {
+    "class": nn.BCEWithLogitsLoss,
+    "loss_args":{
+    }
+}
+from loss.loss import DiceLoss
+loss_function = {
+    "class":DiceLoss,
+    "loss_args":{
+        "mean_type":"pixel"
+    }
+}
+from won.loss import FocalLoss
+loss_function = {
+    "class":ComboLoss,
+    "loss_args":{
+        "weights": {
+            "bce":3,
+            "dice":1,
+            "focal":4
+        }
+    }
+}
+
+num_classes = 1
+from loss.loss import DiceLoss
+loss_function = {
+    "class": DiceLoss,
+    "loss_args":{
+        "mean_type":"pixel",
+        "activation":nn.Sigmoid()
+    }
+}
+
+from loss.loss import FocalLoss
+loss_function = {
+    "class": FocalLoss,
+    "loss_args":{
+        "alpha": 0.98,
+        "gamma": 2
+    }
+}
+
+
+#optimizer
+optimizer = {
+    "class":Adam,
+    "optimizer_args":{
+    }
+}
+
+#dataset
+dataset = {
+    "class": BrainTumorDataset,
+    "dataset_args":{
+        "input_folder":"E:\data\BrainTumor"
+    }
+}
+dataset = {
+    "class": BrainTumorDataset,
+    "dataset_args":{
+        "input_folder":"/content/data/BrainTumor",
+        "augmentation": A.Compose([
+            A.Resize(512, 512),
+            RandomCrop(450, 450),
+            RandomVerticalFlip(p=0.5),
+            RandomHorizontalFlip(p=0.5)
+        ])
+    }
+}
+
+dataset = {
+    "class": BrainTumorDataset,
+    "dataset_args":{
+        "input_folder":"E:\data\BrainTumor",
+        "augmentation": A.Compose([
+            A.Resize(512, 512),
+            RandomCrop(450, 450),
+            RandomVerticalFlip(p=0.5),
+            RandomHorizontalFlip(p=0.5),
+            RandomRotate((0, 270), p = 0.5),
+            RandomBlur(blur_limit = 10, p = 0.5)
+        ])
+    }
+}
+
+import albumentations as A
+from dataset.transform import *
+dataset = {
+    "class": BrainTumorDataset,
+    "dataset_args":{
+        "input_folder":"/content/data/BrainTumor",
+        "augmentation": A.Compose([
+            A.Resize(512, 512),
+            RandomCrop(450, 450, p = 0.5),
+            A.OneOf([
+                RandomVerticalFlip(p=0.5),
+                RandomHorizontalFlip(p=0.5),
+                RandomTranspose(p = 0.5),
+            ]),
+            RandomRotate((0, 270), p = 0.5),
+            RandomBlur(blur_limit = 10, p = 0.1),
+            CLAHE(p = 0.1),
+            RandomBrightnessContrast(p = 0.1)
+        ])
+    }
+}
