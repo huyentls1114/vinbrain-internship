24ede9990b5d26f4aedde6c2ae0354dd6188c74d
huyen <huyentls1114@gmail.com>
2021-02-26 20:44:33 +0700

    classify

--- classifier/model/loss.py
+++ classifier/model/loss.py
@@ -15,3 +15,22 @@ class FocalLoss(nn.Module):
         # print(inputs.shape, targets.shape)
 
         return sigmoid_focal_loss(inputs, targets.float(), self.alpha, self.gamma, reduction="mean")
+
+
+class FocalLoss2d(nn.Module):
+    def __init__(self, gamma=2, ignore_index=255):
+        super().__init__()
+        self.gamma = gamma
+        self.ignore_index = ignore_index
+
+    def forward(self, outputs, targets):
+        outputs = outputs.contiguous()
+        targets = targets.contiguous()
+        eps = 1e-8
+        non_ignored = targets.view(-1) != self.ignore_index
+        targets = targets.view(-1)[non_ignored].float()
+        outputs = outputs.contiguous().view(-1)[non_ignored]
+        outputs = torch.clamp(outputs, eps, 1. - eps)
+        targets = torch.clamp(targets, eps, 1. - eps)
+        pt = (1 - targets) * (1 - outputs) + targets * outputs
+        return (-(1. - pt) ** self.gamma * torch.log(pt)).mean()
