3f66e923c3821f45dae5a92e85ca91e7c32f571a
huyen <huyentls1114@gmail.com>
2021-02-26 18:14:58 +0700

    classify

--- classifier/model/loss.py
+++ classifier/model/loss.py
@@ -10,6 +10,7 @@ class FocalLoss(nn.Module):
         self.gamma = gamma
     def forward(self, inputs, targets):
         assert inputs.shape[1] <= 1
+        print(type(inputs), type(targets))
         inputs = inputs[:, 0]
 
         return sigmoid_focal_loss(inputs, targets, self.alpha, self.gamma, reduction="mean")
