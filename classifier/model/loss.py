bfa28359dc6548b47bef1877d47dd663d06a6975
huyen <huyentls1114@gmail.com>
2021-02-26 16:19:51 +0700

    classify

--- classifier/model/loss.py
+++ classifier/model/loss.py
@@ -1,6 +1,11 @@
 import torch.nn as nn
 import torch.functional as F
 
-def cross_entropy_loss(x, y):
-    #st
-    i = 0
+from torchvision.ops import sigmoid_focal_loss
+class FocalLoss(nn.Module):
+    def __init__(self, alpha = 0.25, gamma = 2):
+        super(FocalLoss, self).__init__()
+        self.alpha = alpha
+        self.gamma = gamma
+    def forward(self, inputs, targets):
+        return sigmoid_focal_loss(inputs, targets, self.alpha, self.gamma, reduction="mean")
