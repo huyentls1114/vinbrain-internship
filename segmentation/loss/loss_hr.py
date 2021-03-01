7a1a19eebf3478a6c30f800e15c09620d307503c
huyen <huyentls1114@gmail.com>
2021-03-01 18:15:37 +0700

    hr

---
+++ segmentation/loss/loss_hr.py
@@ -0,0 +1,18 @@
+import torch.nn as nn
+class CrossEntropy(nn.Module):
+    def __init__(self, ignore_label=-1, weight=None):
+        super(CrossEntropy, self).__init__()
+        self.ignore_label = ignore_label
+        self.criterion = nn.CrossEntropyLoss(weight=weight, 
+                                             ignore_index=ignore_label)
+
+    def forward(self, score, target):
+        ph, pw = score.size(2), score.size(3)
+        h, w = target.size(1), target.size(2)
+        if ph != h or pw != w:
+            score = F.upsample(
+                    input=score, size=(h, w), mode='bilinear')
+
+        loss = self.criterion(score, target)
+
+        return loss
