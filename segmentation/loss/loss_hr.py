f28a554331d363597c9fee6aa46d9950780f3737
huyen <huyentls1114@gmail.com>
2021-03-01 18:32:28 +0700

    hr

--- segmentation/loss/loss_hr.py
+++ segmentation/loss/loss_hr.py
@@ -10,6 +10,7 @@ class CrossEntropy(nn.Module):
     def forward(self, score, target):
         ph, pw = score.size(2), score.size(3)
         h, w = target.size(1), target.size(2)
+        print(score.shape, target.shape)
         if ph != h or pw != w:
             score = F.upsample(
                     input=score, size=(h, w), mode='bilinear')
