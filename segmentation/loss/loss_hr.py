9643407642392008f847c81f016c2dcee9ae2750
huyen <huyentls1114@gmail.com>
2021-03-01 18:17:13 +0700

    hr

--- segmentation/loss/loss_hr.py
+++ segmentation/loss/loss_hr.py
@@ -1,4 +1,6 @@
+import torch
 import torch.nn as nn
+from torch.nn import functional as F
 class CrossEntropy(nn.Module):
     def __init__(self, ignore_label=-1, weight=None):
         super(CrossEntropy, self).__init__()
