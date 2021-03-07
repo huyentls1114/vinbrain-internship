fabfc3b25ed16cda9a3418c104e4379e90e6c7d8
huyen <huyentls1114@gmail.com>
2021-03-08 01:10:47 +0700

    lung segment

---
+++ segmentation/config/config_lungsegment.py
@@ -0,0 +1,127 @@
+import torch.nn as nn
+import torchvision.transforms as transforms
+from torch.optim import Adam
+from torch.optim.lr_scheduler import ReduceLROnPlateau
+
+from dataset.BrainTumorDataset import BrainTumorDataset
+from model.metric import Dice_Score
+from model.unet import Unet, UnetDynamic
+from model.backbone_densenet import BackboneDense121
+from model.backbone import BackboneResnet18VGG, BackboneDensenet121VGG,BackboneEfficientB0VGG
+from utils.utils import len_train_datatset
+from torch.optim.lr_scheduler import OneCycleLR
+import albumentations as A
+import segmentation_models_pytorch as smp
+
+#data config
+image_size = 320
+output_folder = "/content/drive/MyDrive/vinbrain_internship/model_LungSegment/Enet_BCE_augment_RLOP1e-4"
+loss_file = "loss_file.txt"
+config_file_path = "/content/drive/MyDrive/vinbrain_internship/configs/config_lungsegment.py"
+
+#net
+from model.enet import ENet
+net = {
+    "class":ENet,
+    "net_args":{
+        "num_classes":2
+    }
+}
+
+transform_train = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize(mean = (0.540,0.540,0.540), std = (0.264,0.264,0.264)),
+    transforms.Resize(image_size)
+])
+transform_test = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize(mean = (0.540,0.540,0.540), std = (0.264,0.264,0.264)),
+    transforms.Resize(image_size)
+])
+transform_label = transforms.Compose([
+    transforms.ToTensor(),
+    # transforms.Normalize(mean = (0.540,0.540,0.540), std = (0.264,0.264,0.264)),
+    transforms.Resize(image_size)
+])
+
+import albumentations as A
+from dataset.transform import *
+from dataset.LungDataset import *
+dataset = {
+    "class": LungDataset,
+    "dataset_args":{
+        "covid_chesxray_folder":"/content/data/Pneumothorax",
+        "augmentation": A.Compose([
+            A.Resize(576, 576),
+            RandomRotate((-30, 30), p = 0.5),
+            A.OneOf([
+                # RandomVerticalFlip(p=0.5),
+                RandomHorizontalFlip(p=0.5),
+                # RandomTranspose(p = 0.5),
+            ]),
+            RandomBlur(blur_limit = 3.1, p = 0.1),
+            # CLAHE(p = 0.1),
+            RandomBrightnessContrast(p = 0.1),
+            RandomCrop(512, 512, p = 0.5)
+        ]),
+        "update_ds": {
+            "weight_positive": 1
+        }
+    }
+}
+
+
+device = "gpu"
+gpu_id = 0
+
+batch_size = 16
+num_epochs = 20
+
+# from pattern_model import 
+from won.loss import DiceMetric
+metric = {
+    "class":DiceMetric,
+    "metric_args":{
+        "num_classes": num_classes,
+        "threshold": 0.5
+    }
+}
+
+loss_function = {
+    "class": nn.CrossEntropyLoss,
+    "loss_args":{
+    }
+}
+
+
+#optimizer
+lr = 1e-4
+optimizer = {
+    "class":Adam,
+    "optimizer_args":{
+    }
+}
+
+from torch.optim.lr_scheduler import ReduceLROnPlateau
+lr_scheduler = {
+    "class": ReduceLROnPlateau,
+    "metric":"val_loss_avg",
+    "step_type":"epoch",
+    "schedule_args":{
+        "mode":"min",
+        "factor":0.3,
+        "patience":4,
+        "threshold":1e-2,
+        "min_lr":1e-6
+    }
+}
+from torch.optim.lr_scheduler import CosineAnnealingWarmRestarts
+lr_scheduler_crf = {
+    "class":CosineAnnealingWarmRestarts,
+    "metric": None,
+    "step_type":"iteration",
+    "schedule_args":{
+        "T_0": 1,
+        "T_mult":2
+    }    
+}
---
+++ segmentation/dataset/LungDataset.py
@@ -0,0 +1,64 @@
+from torch.utils.data import Dataset
+import os
+
+class LungDataset(Dataset):
+    def __init__(self, dataset_args, transform_image, transform_label, mode = "train"):
+        super(self, LungDataset).__init__()
+        if "augmentation" in dataset_args.keys():
+            self.augmentation = dataset_args["augmentation"]
+        else:
+            self.augmentation = None
+
+
+        self.covid_chesxray_folder = dataset_args["covid_chesxray_folder"]
+        self.covid_chesxray_image_folder = os.path.join(self.covid_chesxray_folder, "images")
+        self.covid_chesxray_mask_folder = os.path.join(self.covid_chesxray_folder, "masks")
+        self.covid_chesxray_names = self.read_txt(self.covid_chesxray_folder, mode)
+
+        self.transform_image = transform_image
+        self.transform_label = transform_label
+
+    def __len__(self):
+        return len(self.list_img_name)
+    
+    def __getitem__(self, idx):
+        img_name = self.covid_chesxray_names[idx]
+        img_path = os.path.join(self.covid_chesxray_image_folder, img_name)
+        image = plt.imread(img_path)
+        # image = image[:, :, 0]
+
+        mask_path = os.path.join(self.covid_chesxray_mask_folder, img_name)
+        mask = plt.imread(mask_path)
+        mask = mask[:, :, 0]
+
+        if (self.mode == "train") and (self.augmentation is not None):
+            # print(self.mode)
+            augmented = self.augmentation(image = image, mask = mask)
+            image, mask = augmented['image'], augmented['mask']
+        return self.transform_image(np.array(image)), self.transform_label(np.array(mask))
+        
+    def load_sample(self, batch_size = 4):
+        list_imgs = []
+        list_masks = []
+
+        #random list idx
+        list_idx = np.arange(self.__len__())
+        np.random.shuffle(list_idx)
+        list_idx = list_idx[0:batch_size]
+
+        for i in range(batch_size):
+            image, mask = self.__getitem__(list_idx[i])
+            list_imgs.append(image[None, :, :])
+            list_masks.append(mask[None, :, :])
+        return torch.cat(list_imgs), torch.cat(list_masks)
+
+    def read_txt(self, input_folder, mode):
+        train_txt_file = os.path.join(input_folder, "%s.txt"%(mode))
+        file_ = open(train_txt_file)
+        list_ = file_.readlines()
+        list_img_name = []
+        for line in list_:
+            img_name = line.replace("\n","")
+            list_img_name.append(img_name)
+        file_.close()
+        return list_img_name
