feb89ad35d6f7547b17122f275a7b8b2f41d5e19
huyen <huyentls1114@gmail.com>
2021-03-08 02:06:05 +0700

    lung segment

--- segmentation/dataset/LungDataset.py
+++ segmentation/dataset/LungDataset.py
@@ -104,6 +104,6 @@ def process_img(image, channel = 3):
         return image[:,:, 0:1]
     if image.shape[2] == channel:
         return image
-    if iamge.shape[2] > channel:
+    if image.shape[2] > channel:
         return image[:,:,:channel]
     
