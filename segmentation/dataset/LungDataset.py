510bb634a317d22f7bbd31e99a23037cc3ad6d8f
huyen <huyentls1114@gmail.com>
2021-03-08 02:05:34 +0700

    lung segment

--- segmentation/dataset/LungDataset.py
+++ segmentation/dataset/LungDataset.py
@@ -95,7 +95,7 @@ class LungDataset(Dataset):
 def process_img(image, channel = 3):
     "return image with shape [w, h, channel]"
     print(image.shape)
-    if image.shape == 2:
+    if len(image.shape) == 2:
         image = np.expand_dims(image, axis=-1)
         return np.dstack((image, )*channel)
     if (channel >1) and (image.shape[2] == 1):
