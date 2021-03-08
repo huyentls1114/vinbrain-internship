31a13ec16996c4b5594d56329b7bc1daae731246
huyen <huyentls1114@gmail.com>
2021-03-08 12:21:00 +0700

    lung segment

--- segmentation/dataset/LungDataset.py
+++ segmentation/dataset/LungDataset.py
@@ -78,7 +78,7 @@ class LungDataset(Dataset):
             # import pdb; pdb.set_trace()
         # print(image.shape)
         # import pdb; pdb.set_trace()
-        return self.transform_image(np.array(image)), self.transform_label(np.array(mask))
+        return self.transform_image(np.array(image)), self.transform_label(np.array(mask)).long()
         
     def load_sample(self, batch_size = 4):
         list_imgs = []
