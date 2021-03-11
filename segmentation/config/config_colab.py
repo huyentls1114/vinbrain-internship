ab793ae78c187e798f3cd655bd909fa19fef870a
huyen <huyentls1114@gmail.com>
2021-03-11 18:55:55 +0700

    unet-resnet101

--- segmentation/config/config_colab.py
+++ segmentation/config/config_colab.py
@@ -15,7 +15,7 @@ import segmentation_models_pytorch as smp
 
 #data config
 image_size = 256
-output_folder = "/content/drive/MyDrive/vinbrain_internship/model_Pneumothorax/Unet_BCE_rate0.8_augment_RLOP1e-4"
+output_folder = "/content/drive/MyDrive/vinbrain_internship/model_Pneumothorax/Unet_Resnet101_BCE_rate0.8_augment_RLOP1e-4"
 loss_file = "loss_file.txt"
 config_file_path = "/content/vinbrain-internship/segmentation/config/config_colab.py"
 
