13ee80d96c4cbf429d551501104896d5427eb7a6
huyen <huyentls1114@gmail.com>
2021-03-11 16:02:12 +0700

    pspnet

--- segmentation/loss/loss_psp.py
+++ segmentation/loss/loss_psp.py
@@ -1,14 +1,39 @@
 import torch
 import torch.nn as nn
 from torch.nn import functional as F
+
+class WeightedBCEv2(nn.Module):
+    def __init__(self):
+        super(WeightedBCEv2, self).__init__()
+    #
+    def forward(self, y_pred, y_true, reduction='mean'):
+        y_pred = y_pred[:,1].view(-1)
+        y_true = y_true.view(-1)
+        assert(y_pred.shape==y_true.shape)
+
+        loss = F.binary_cross_entropy(y_pred, y_true, reduction='none')
+
+        pos = (y_true>0.5).float()
+        neg = (y_true<0.5).float()
+        pos_weight = (pos.sum().item() + 1) / len(y_true)
+        neg_weight = (neg.sum().item() + 1) / len(y_true)
+        pos_weight = 1 / pos_weight
+        neg_weight = 1 / neg_weight 
+        pos_weight = np.log(pos_weight) + 1
+        neg_weight = np.log(neg_weight) + 1
+        pos_weight = pos_weight / (pos_weight + neg_weight)
+        neg_weight = neg_weight / (pos_weight + neg_weight)
+        loss = (pos*loss*pos_weight + neg*loss*neg_weight).mean()
+        return loss
+
 class BCEWithLogits_Compose(nn.Module):
     def __init__(self, ignore_label=-1, weight=None, aux_weight = None):
         super(BCEWithLogits_Compose, self).__init__()
         self.ignore_label = ignore_label
         self.aux_weight = aux_weight
 
-        self.main_criterion = nn.BCEWithLogitsLoss(weight=weight)
-        self.aux_criterion = nn.BCEWithLogitsLoss(weight=weight)
+        self.main_criterion = nn.WeightedBCEv2()
+        self.aux_criterion = nn.WeightedBCEv2()
         
     def forward(self, output, target):
         if isinstance(output, tuple):
